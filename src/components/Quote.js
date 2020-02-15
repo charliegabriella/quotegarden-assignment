fb9ab657745fc72869e9b6ac097f0aec96b5825e
charliegabriella <charlotkroon@gmail.com>
2020-02-15 15:16:18 +0100

    Change color of quote when clicked on

--- src/components/Quote.js
+++ src/components/Quote.js
@@ -3,16 +3,20 @@ import "./Likes.css";
 
 export default class Quote extends Component {
   state = {
-    numLikes: 0
+    numLikes: 0,
+    color: "black"
   };
+
   incrementLikes = () => {
     this.setState({
+      color: "pink",
       numLikes: this.state.numLikes + 1
     });
   };
 
   decrementLikes = () => {
     this.setState({
+      color: "grey",
       numLikes: this.state.numLikes - 1
     });
   };
@@ -21,8 +25,13 @@ export default class Quote extends Component {
     const {} = this.props;
     return (
       <div>
-        <div>{this.props.quoteText}</div>
-        <div>Wisely spoken by: {this.props.quoteAuthor}</div>
+        <div>
+          <h3 style={{ color: this.state.color }}> {this.props.quoteText}</h3>{" "}
+          {/* change the color of the text when it's*/}
+        </div>
+        <div>
+          <h4>Wisely spoken by: {this.props.quoteAuthor}</h4>
+        </div>
         <br />
         <div className="likeElement">
           <div className="Display">
@@ -32,7 +41,6 @@ export default class Quote extends Component {
           <div className="arrow-up" onClick={this.incrementLikes}></div>
           <div className="arrow-down" onClick={this.decrementLikes}></div>
         </div>
-        <br />
       </div>
     );
   }
