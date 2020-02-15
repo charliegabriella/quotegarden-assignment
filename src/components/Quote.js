28bfaabe611176b26388044469d9d43a63d51d5f
charliegabriella <charlotkroon@gmail.com>
2020-02-15 13:52:46 +0100

    Added up and down arrows/like feature/A like counter

--- src/components/Quote.js
+++ src/components/Quote.js
@@ -1,13 +1,38 @@
 import React, { Component } from "react";
+import "./Likes.css";
 
 export default class Quote extends Component {
+  state = {
+    numLikes: 0
+  };
+  incrementLikes = () => {
+    this.setState({
+      numLikes: this.state.numLikes + 1
+    });
+  };
+
+  decrementLikes = () => {
+    this.setState({
+      numLikes: this.state.numLikes - 1
+    });
+  };
+
   render() {
     const {} = this.props;
     return (
       <div>
         <div>{this.props.quoteText}</div>
-        <br />
         <div>Wisely spoken by: {this.props.quoteAuthor}</div>
+        <br />
+        <div className="likeElement">
+          <div className="Display">
+            This quote has <b>{this.state.numLikes}</b> likes!
+          </div>
+          <br />
+          <div className="arrow-up" onClick={this.incrementLikes}></div>
+          <div className="arrow-down" onClick={this.decrementLikes}></div>
+        </div>
+        <br />
       </div>
     );
   }
